144c5daaa0ce481789bbbcf75e48b76f7b355d17
Carlos Pantoja <carlos6.andre0@gmail.com>
2026-05-06 15:13:54 -0300

    modificações

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,24 @@
 import { Link } from "react-router-dom"
 
 export function Nav() {
+    function dropbox() {
+        if(document.getElementById('dropbox-content').style.display === 'none') {
+            return document.getElementById('dropbox-content').style.display = 'flex'
+        } else {
+            return document.getElementById('dropbox-content').style.display = 'none'
+        }
+    }
+
     return(
         <nav>
-            <Link to={'/'}>Home</Link>
-            <Link to={'/calculadora'}>Calculadora</Link>
-            <Link to={'/jogo-da-velha'}>Jogo da Velha</Link>
+            <div id="dropbox" onClick={dropbox}>
+                <i className="fa fa-bars"></i>
+                <div id="dropbox-content">
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'/calculadora'}>Calculadora</Link>
+                    <Link to={'/jogo-da-velha'}>Jogo da Velha</Link>
+                </div>
+            </div>
         </nav>
     )
 }
